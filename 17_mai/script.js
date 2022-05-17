cf3c80e49525e835e85464c32c3472764b1c912e
othniel <kouamefranckolivier45@gmail.com>
2022-05-17 17:36:33 +0000

    my first commit

---
+++ 17_mai/script.js
@@ -0,0 +1,10 @@
+//colors array
+let colors =('blue, yellow, red, brown, orange');
+
+//get button
+let button = document.gentlementById('button');
+
+//add event listener
+button.addEventlistener('click', function(){
+    
+}
